5ba459de5a24de8fb69e2fc4e2817ac94f24e592
Aaron Brayall <abrayall@yahoo.com>
2017-04-11 11:12:39 -0400

    updating javax.lang.System with object based print functionality

--- src/main/java/javax/lang/System.java
+++ src/main/java/javax/lang/System.java
@@ -3,8 +3,24 @@ package javax.lang;
 import static javax.lang.Strings.*;
 
 public class System {
+	public static void print(Object... objects) {
+		println(strings(objects));
+	}
+	
+	public static void println(Object... objects) {
+		println(strings(objects));
+	}
+	
+	public static void print(String... strings) {
+		print(join(strings));
+	}
+	
 	public static void println(String... strings) {
 		println(join(strings));
+	}
+		
+	public static void print(String string) {
+		java.lang.System.out.print(string);
 	}
 	
 	public static void println(String string) {
