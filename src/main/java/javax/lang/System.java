900deeb7c2dcf71b2d3ee63317e64882c94e8abe
Aaron Brayall <abrayall@yahoo.com>
2017-07-05 14:56:22 -0400

    updating javax.lang.System to fix issue with getProperty

--- src/main/java/javax/lang/System.java
+++ src/main/java/javax/lang/System.java
@@ -154,7 +154,7 @@ public class System {
 	}
 	
 	public static String getProperty(String name) {
-		return getProperty(null);
+		return getProperty(name, null);
 	}
 	
 	public static String getProperty(String name, String defaultValue) {
