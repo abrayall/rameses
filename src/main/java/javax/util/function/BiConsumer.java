7a94663d29a96de3cba1ffc4dffe93c3e9a9f513
Aaron Brayall <abrayall@yahoo.com>
2017-07-06 17:04:27 -0400

    adding javax.util.function.Consumer and BiConsumer

---
+++ src/main/java/javax/util/function/BiConsumer.java
@@ -0,0 +1,4 @@
+package javax.util.function;
+
+@FunctionalInterface
+public interface BiConsumer<T, U> extends java.util.function.BiConsumer<T, U> {}
---
+++ src/main/java/javax/util/function/Consumer.java
@@ -0,0 +1,4 @@
+package javax.util.function;
+
+@FunctionalInterface
+public interface Consumer<T> extends java.util.function.Consumer<T> {}
