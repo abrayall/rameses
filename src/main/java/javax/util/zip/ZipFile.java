247f61f09b1653fdce26b7a9752ac991eccb40e8
Aaron Brayall <abrayall@yahoo.com>
2017-07-06 20:50:06 -0400

    updating javax.util.zip.ZipFile so callers can pass in a function to control extacting individual entries

--- src/main/java/javax/util/zip/ZipFile.java
+++ src/main/java/javax/util/zip/ZipFile.java
@@ -8,6 +8,7 @@ import java.util.Iterator;
 import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 import java.util.zip.ZipEntry;
@@ -33,20 +34,26 @@ public class ZipFile extends java.util.zip.ZipFile {
 		return extract(file(path));
 	}
 	
-	public ZipFile extract(java.io.File path, BiConsumer<ZipEntry, File> progress) throws Exception {
-		return extract(file(path), progress);
+	public ZipFile extract(java.io.File path, BiFunction<ZipEntry, File, File> handler) throws Exception {
+		return extract(file(path), handler);
 	}
 	
 	public ZipFile extract(File path) throws Exception {
-		return extract(path, (entry, file) -> {});
+		return extract(path, (entry, file) -> file);
 	}
 	
-	public ZipFile extract(File path, BiConsumer<ZipEntry, File> progress) throws Exception {
+	public ZipFile extract(File path, BiConsumer<ZipEntry, File> handler) throws Exception {
+		return extract(path, (entry, file) -> {
+			handler.accept(entry,  file);
+			return file;
+		});
+	}
+	
+	public ZipFile extract(File path, BiFunction<ZipEntry, File, File> handler) throws Exception {
 		stream(this.entries()).forEach(entry -> {
 			File file = file(path, entry.getName());
 			try {
-				extract(entry, file);
-				progress.accept(entry, file);
+				extract(entry, handler.apply(entry, file));
 			} catch (Exception e) {
 			}
 		});
@@ -55,9 +62,9 @@ public class ZipFile extends java.util.zip.ZipFile {
 	}
 	
 	public ZipFile extract(ZipEntry entry, File path) throws Exception {
-		if (entry.isDirectory())
+		if (entry != null && entry.isDirectory())
 			path.mkdirs();
-		else
+		else if (entry != null)
 			copy(this.getInputStream(entry), path.mkdirs().outputStream()).close();
 		
 		return this;
@@ -102,6 +109,13 @@ public class ZipFile extends java.util.zip.ZipFile {
 	}
 	
 	public static File extract(File file, File path, BiConsumer<ZipEntry, File> progress) throws Exception {
+		return extract(file, path, (e, f) -> {
+			progress.accept(e, f);
+			return f;
+		});
+	}
+	
+	public static File extract(File file, File path, BiFunction<ZipEntry, File, File> progress) throws Exception {
 		zip(file).extract(path, progress).close();
 		return path;
 	}
